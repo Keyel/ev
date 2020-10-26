b46d970f18db7819f2761a6eb2fddaaf81d78d03
Keyel <keyel74@gmail.com>
2020-10-26 11:38:18 +0100

    body parser json + postman test file

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -4,7 +4,21 @@ export
 const apiRouter = express.Router()
 
 apiRouter.get('/hi', (request, response) => {
-    response.send('Hello API world!');
+    response.send('Hello API Bandi!');
 });
+
+apiRouter.get('/whoami', (request, response) => {
+    response.send( {
+        router: "api",
+        hostname: request.hostname,
+        path: request.path,
+        method: request.method
+    });
+});
+
+apiRouter.post('/', (request, response) => {
+    response.send(request.body);
+});
+
 
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,22 @@
-import * as express from 'express';
+import * as express from 'express'
 import {apiRouter} from './api'
+import * as bodyParser from 'body-parser'
  
-function loggerMiddleware(request: express.Request, response: express.Response, next) {
+function loggerMiddleware(request: express.Request, response: express.Response, next: () => void) {
     console.log(`${request.method} ${request.path}`);
     next();
 }
 
 const app = express()
 
-app.use(loggerMiddleware)
-app.use("/api", apiRouter)
+app.use(loggerMiddleware) //logolja a bejovo hivasokat
+app.use(bodyParser.json()) //elerhetove teszi a jsonoket a bodyban (request.body neven)
+app.use("/api", apiRouter) //regisztral egy routert az api pathra
 
 app.get('/', (request, response) => {
-    response.send('There is no static content at the moment, use the api istead!');
+    response.send('There is no static content at the moment, use the api instead!');
 });
  
-app.listen(5000);
+app.listen(5000, () => {
+    console.log("Listening on 5000")
+});
